9cc9d347b601dc2c26a826ba0bad5700f5659663
WangJing <wangjing@mprc.pku.edu.cn>
2011-10-25 18:00:10 +0800

    UniCore64: Add function update_mmu_cache
    Fixed error:
    undefined reference to `update_mmu_cache'
    
    Signed-off-by: WangJing <wangjing@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/mm/mmu.c
+++ patches-3.7/arch/unicore64/mm/mmu.c
@@ -1,5 +1,32 @@
+#include <linux/mm.h>
+
 /*
  * empty_zero_page is a special page that is used for
  * zero-initialized data and COW.
  */
 struct page *empty_zero_page;
+
+/**
+ * update_mmu_cache() - checks the page table entry for validity
+ * @vma:
+ * @adddr:
+ * @ptep:
+ *
+ * Take care of architecture specific things when placing a new PTE into
+ * a page table, or changing an existing PTE.  Basically, there are two
+ * things that we need to take care of:
+ *
+ *  1. If PG_dcache_clean is not set for the page, we need to ensure
+ *     that any cache entries for the kernels virtual memory
+ *     range are written back to the page.
+ *  2. If we have multiple shared mappings of the same space in
+ *     an object, we need to deal with the cache aliasing issues.
+ *
+ * Note that the pte lock will be held.
+ */
+void update_mmu_cache(struct vm_area_struct *vma, unsigned long addr,
+	pte_t *ptep)
+{
+	/* FIXME */
+	BUG();
+}
