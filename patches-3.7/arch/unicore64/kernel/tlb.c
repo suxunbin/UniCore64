aeee547e2531bddad6ab407cf9f9fd31882c7c18
WangJing <wangjing@mprc.pku.edu.cn>
2011-10-26 17:04:16 +0800

    UniCore64: Add tlb flush functions
    Fixed errors:
    kernel/built-in.o: In function `dup_mm':
    (.text+0x46cd): undefined reference to `flush_tlb_mm'
    mm/built-in.o: In function `handle_pte_fault':
    (.text+0x18488): undefined reference to `flush_tlb_page'
    mm/built-in.o: In function `unmap_kernel_range':
    (.text+0x23086): undefined reference to `flush_tlb_kernel_range'
    mm/built-in.o: In function `mprotect_fixup':
    (.text+0x1ec32): undefined reference to `flush_tlb_range'
    
    Signed-off-by: WangJing <wangjing@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/kernel/tlb.c
@@ -0,0 +1,49 @@
+#include <linux/mm.h>
+#include <linux/kernel.h>
+#include <asm/tlbflush.h>
+
+/**
+ * flush_tlb_mm() -
+ * @mm
+ */
+void flush_tlb_mm(struct mm_struct *mm)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * flush_tlb_page() -
+ * @vma:
+ * @va:
+ */
+void flush_tlb_page(struct vm_area_struct *vma, unsigned long va)
+{
+	/* FIXME */
+	BUG();
+}
+
+
+/**
+ * flush_tlb_range() -
+ * @vma:
+ * @start:
+ * @end:
+ */
+void flush_tlb_range(struct vm_area_struct *vma,
+		     unsigned long start, unsigned long end)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * flush_tlb_kernl_range() -
+ * @start:
+ * @end:
+ */
+void flush_tlb_kernel_range(unsigned long start, unsigned long end)
+{
+	/* FIXME */
+	BUG();
+}
