7b30e172ecc43b1aa173cc3ce2b5ce4673e9c439
WangJing <wangjing@mprc.pku.edu.cn>
2011-10-26 17:04:18 +0800

    UniCore64: Add mem_init and free_initmem function
    Fixed errors:
    init/built-in.o: In function `init_post':
    main.c:(.text+0x2a): undefined reference to `free_initmem'
    init/built-in.o: In function `start_kernel':
    (.init.text+0x4ea): undefined reference to `mem_init'
    
    Signed-off-by: WangJing <wangjing@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/mm/init.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+
+/**
+ * mem_init() -
+ */
+void __init mem_init(void)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * free_initmem() -
+ */
+void free_initmem(void)
+{
+	/* FIXME */
+	BUG();
+}
