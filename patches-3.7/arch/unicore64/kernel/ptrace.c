a55fe967f8801438001357fb34c12cd79c27770e
Yu Yue <yuyue@mprc.pku.edu.cn>
2011-10-28 19:15:38 +0800

    UniCore64: Add function arch_ptrace and ptrace_disable
    Fixed error:
    elfcore.c:(.text+0x18638): undefined reference to `arch_ptrace'
    elfcore.c:(.text+0x18dc4): undefined reference to `ptrace_disable'
    
    Signed-off-by: Yu Yue <yuyue@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/kernel/ptrace.c
@@ -0,0 +1,23 @@
+#include <linux/kernel.h>
+#include <linux/ptrace.h>
+
+/**
+ * ptrace_disable() - Called by kernel/ptrace.c when detaching
+ * @child:
+ */
+void ptrace_disable(struct task_struct *child)
+{
+}
+
+/**
+ * arch_ptrace() -
+ * @child:
+ * @request:
+ * @addr:
+ * @data:
+ */
+long arch_ptrace(struct task_struct *child, long request,
+		 unsigned long addr, unsigned long data)
+{
+	return 0;
+}
