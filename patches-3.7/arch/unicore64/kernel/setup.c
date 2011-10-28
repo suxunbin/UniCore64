e8a7ec6c444b7cb942e69be3cc2894b09fd80de1
Yu Yue <yuyue@mprc.pku.edu.cn>
2011-10-28 19:15:39 +0800

    UniCore64: Add function setup_arch
    Fixed Error:
    calibrate.c:(.init.text+0xa3c): undefined reference to `setup_arch'
    
    Signed-off-by: Yu Yue <yuyue@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/kernel/setup.c
@@ -0,0 +1,12 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/**
+ * setup_arch() - Architecture-specific boot-time initializations
+ * @cmdline_p:
+ */
+void __init setup_arch(char **cmdline_p)
+{
+	/* FIXME */
+	BUG();
+}
