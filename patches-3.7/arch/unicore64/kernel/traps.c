e56acd4e1efa9847e0c2c0ebe0e75a1b79885a83
Yu Yue <yuyue@mprc.pku.edu.cn>
2011-10-31 17:58:44 +0800

    UniCore64: Add functions show_stack and trap_init
    Fixed error:
    elfcore.c:(.text+0x35e0): undefined reference to `show_stack'
    calibrate.c:(.init.text+0xaf8): undefined reference to `trap_init'
    
    Signed-off-by: Yu Yue <yuyue@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/kernel/traps.c
@@ -0,0 +1,22 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/**
+ * trap_init() -
+ */
+void __init trap_init(void)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * show_stack() -
+ * @tsk:
+ * @sp:
+ */
+void show_stack(struct task_struct *tsk, unsigned long *sp)
+{
+	/* FIXME */
+	BUG();
+}
