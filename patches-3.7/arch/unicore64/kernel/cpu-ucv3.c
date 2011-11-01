46b01ae3fb0b36f48bc192584bf89aaac60ac0dc
Yu Yue <yuyue@mprc.pku.edu.cn>
2011-11-01 13:48:21 +0800

    UniCore64: Add struct cpuinfo_op
    Fixed error:
    file-mmu.c:(.text+0x691f8): undefined reference to `cpuinfo_op'
    
    Signed-off-by: Yu Yue <yuyue@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/kernel/cpu-ucv3.c
@@ -0,0 +1,65 @@
+#include <linux/kernel.h>
+#include <linux/seq_file.h>
+
+/**
+ * cpu_ucv3_show() -
+ * @m:
+ * @v:
+ */
+static int cpu_ucv3_show(struct seq_file *m, void *v)
+{
+	/* FIXME */
+	BUG();
+	return 0;
+}
+
+/**
+ * cpu_ucv3_start() -
+ * @m:
+ * @pos:
+ */
+static void *cpu_ucv3_start(struct seq_file *m, loff_t *pos)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * cpu_ucv3_next() -
+ * @v:
+ * @pos:
+ */
+static void *cpu_ucv3_next(struct seq_file *m, void *v, loff_t *pos)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * cpu_ucv3_stop() -
+ * @m:
+ * @v:
+ */
+static void cpu_ucv3_stop(struct seq_file *m, void *v)
+{
+	/* FIXME */
+	BUG();
+}
+
+/**
+ * struct cpuinfo_op -
+ * @start:
+ * @next:
+ * @stop:
+ * @show:
+ */
+const struct seq_operations cpuinfo_op = {
+	.start	= cpu_ucv3_start,
+	.next	= cpu_ucv3_next,
+	.stop	= cpu_ucv3_stop,
+	.show	= cpu_ucv3_show
+};
+
+#ifdef CONFIG_CPU_FREQ
+	/* FIXME */
+#endif
