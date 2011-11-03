493a4f22cf5afeee9fe341c7b4c3efe69aac5fea
Liu Guoli <liuguoli@mprc.pku.edu.cn>
2011-11-03 09:17:06 +0800

    UniCore64: Add arch/memory.h for KERNEL_IMAGE_START
    Signed-off-by: Liu Guoli <liuguoli@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/include/arch/memory.h
@@ -0,0 +1,11 @@
+#ifndef __ARCH_MEMORY_H__
+#define __ARCH_MEMORY_H__
+
+#ifdef __ASSEMBLY__
+
+/* The byte offset of the kernel image in RAM from the start of RAM. */
+#define KERNEL_IMAGE_START	0x0000000000408000
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* __ARCH_MEMORY_H__ */
