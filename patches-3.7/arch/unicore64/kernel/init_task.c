a5afd4a54e6c844bb4cd0bef6ccc43827db8e9d3
yuyue <yuyue@mprc.pku.edu.cn>
2011-11-03 13:49:28 +0800

    UniCore64: Add union init_thread_union
    Fixed error:
    arch/unicore64/kernel/built-in.o:(.data+0x8): undefined reference to `init_thread_union'
    
    Signed-off-by: yuyue <yuyue@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/kernel/init_task.c
+++ patches-3.7/arch/unicore64/kernel/init_task.c
@@ -2,6 +2,20 @@
 #include <linux/sched.h>
 #include <linux/init_task.h>
 #include <linux/mqueue.h>
+#include <linux/hardirq.h>
+
+/**
+ * union init_thread_union - Initial thread structure.
+ *
+ * We need to make sure that this is 8192-byte aligned due to the
+ * way process stacks are handled. This is done by making sure
+ * the linker maps this in the .text segment right after head.S,
+ * and making head.S ensure the proper alignment.
+ *
+ * The things we do for performance..
+ */
+union thread_union init_thread_union __init_task_data = {
+	INIT_THREAD_INFO(init_task) };
 
 /**
  * struct init_signals -
