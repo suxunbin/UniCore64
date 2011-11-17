2f1031cc1468c76227e2e839363cd03631640413
Guan Xuetao <gxt@mprc.pku.edu.cn>
2011-11-17 09:47:15 +0800

    UniCore64: add arch/unicore64/include/arch/bitfield.h
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/include/arch/bitfield.h
@@ -0,0 +1,20 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef __UNICORE64_ARCH_BITFIELD_H__
+#define __UNICORE64_ARCH_BITFIELD_H__
+
+#ifndef __ASSEMBLY__
+#define UData(Data)	((unsigned long) (Data))
+#else
+#define UData(Data)	(Data)
+#endif
+
+#define FIELD(val, vmask, vshift)		\\
+		(((val) & ((UData(1) << (vmask)) - 1)) << (vshift))
+#define FMASK(vmask, vshift)			\\
+		(((UData(1) << (vmask)) - 1) << (vshift))
+
+#endif /* __UNICORE64_ARCH_BITFIELD_H__ */
