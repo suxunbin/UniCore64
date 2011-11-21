1aef98d031aad94779dcab71ecb9e2834ec14b81
Guan Xuetao <gxt@mprc.pku.edu.cn>
2011-11-21 14:00:57 +0800

    UniCore64: replace UData with _AC macro
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/arch/bitfield.h
+++ patches-3.7/arch/unicore64/include/arch/bitfield.h
@@ -6,15 +6,11 @@
 #ifndef __UNICORE64_ARCH_BITFIELD_H__
 #define __UNICORE64_ARCH_BITFIELD_H__
 
-#ifndef __ASSEMBLY__
-#define UData(Data)	((unsigned long) (Data))
-#else
-#define UData(Data)	(Data)
-#endif
+#include <linux/const.h>
 
 #define FIELD(val, vmask, vshift)		\
-		(((val) & ((UData(1) << (vmask)) - 1)) << (vshift))
+		(((val) & ((_AC(1, UL) << (vmask)) - 1)) << (vshift))
 #define FMASK(vmask, vshift)			\
-		(((UData(1) << (vmask)) - 1) << (vshift))
+		(((_AC(1, UL) << (vmask)) - 1) << (vshift))
 
 #endif /* __UNICORE64_ARCH_BITFIELD_H__ */
