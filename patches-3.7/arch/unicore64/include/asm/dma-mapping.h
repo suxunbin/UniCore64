a764f65aa5718428f08f250d3c0d415d133604f9
LiangRongrong <Liangrongrong@mprc.pku.edu.cn>
2011-11-29 21:40:26 +0800

    UniCore64:fix checkpatch errors in asm dma-mapping.h
    ERROR: Macros with complex values should be enclosed in parenthesis
           get_dma_ops(dev)->dma_supported((d), (m))
    
    Signed-off-by: Liang Rongrong <liangrongrong@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/dma-mapping.h
+++ patches-3.7/arch/unicore64/include/asm/dma-mapping.h
@@ -6,11 +6,11 @@ extern struct dma_map_ops swiotlb_dma_map_ops;
 #define get_dma_ops(dev)		(&swiotlb_dma_map_ops)
 
 #define dma_supported(d, m)			\
-		get_dma_ops(dev)->dma_supported((d), (m))
+		(get_dma_ops(dev)->dma_supported((d), (m)))
 #define dma_alloc_coherent(d, s, h, f)		\
-		get_dma_ops(dev)->alloc_coherent((d), (s), (h), (f))
+		(get_dma_ops(dev)->alloc_coherent((d), (s), (h), (f)))
 #define dma_free_coherent(d, s, a, h)		\
-		get_dma_ops(dev)->free_coherent((d), (s), (a), (h))
+		(get_dma_ops(dev)->free_coherent((d), (s), (a), (h)))
 
 #define dma_alloc_noncoherent(d, s, h, f)	\
 		dma_alloc_coherent((d), (s), (h), (f))
