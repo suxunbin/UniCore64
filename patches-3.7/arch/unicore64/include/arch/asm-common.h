b94115380f2f7e72cad06aff905e61e364bf0864
Yu Yue <yuyue@mprc.pku.edu.cn>
2011-11-30 11:35:44 +0800

    UniCore64: add __ASM_WRAP
    Signed-off-by: Yu Yue <yuyue@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/arch/asm-common.h
+++ patches-3.7/arch/unicore64/include/arch/asm-common.h
@@ -3,4 +3,14 @@
 
 #define csub		cmpsub
 
+/*
+ * __ASM_WRAP is ONLY used for assembly macro definitions.
+ * Then this macro could be used in both assembly files and c files.
+ */
+#ifdef __ASSEMBLY__
+#define __ASM_WRAP(code...)	code
+#else
+#define __ASM_WRAP(code...)	__asm__(#code);
+#endif /* __ASSEMBLY__ */
+
 #endif /* __UNICORE64_ARCH_ASM_COMMON_H__ */
