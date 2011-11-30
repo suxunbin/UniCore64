0dbe7ec2dca384ff9d439476299e033f94e8ad72
Liu Guoli <liuguoli@mprc.pku.edu.cn>
2011-11-30 11:36:10 +0800

    UniCore64: Add irqflags.h
    Signed-off-by: Liu Guoli <liuguoli@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/irqflags.h
+++ patches-3.7/arch/unicore64/include/asm/irqflags.h
@@ -1,15 +1,21 @@
 #ifndef __ASM_UNICORE64_IRQFLAGS_H__
 #define __ASM_UNICORE64_IRQFLAGS_H__
 
-#ifdef __KERNEL__
+#include <arch/hwdef-cpu.h>
+
+#define ARCH_IRQ_DISABLED	ASR_INTR_MASK
+#define ARCH_IRQ_ENABLED	0x0
 
 /**
  * arch_local_save_flags() - Save the current interrupt enable state.
  */
 static inline unsigned long arch_local_save_flags(void)
 {
-	/* FIXME */
-	return 0;
+	unsigned long temp;
+
+	asm volatile("dmov %0, asr" : "=r" (temp));
+
+	return temp & ASR_INTR_MASK;
 }
 
 /**
@@ -18,10 +24,17 @@ static inline unsigned long arch_local_save_flags(void)
  */
 static inline void arch_local_irq_restore(unsigned long flags)
 {
-	/* FIXME */
+	unsigned long temp;
+
+	asm volatile(
+		"dmov	%0, asr\n"
+		"dandn	%0, %0, %2\n"
+		"dor	%0, %0, %1\n"
+		"dmov	asr, %0"
+		: "=&r" (temp)
+		: "r" (flags), "i" (ASR_INTR_MASK));
 }
 
 #include <asm-generic/irqflags.h>
 
-#endif /* __KERNEL__ */
 #endif /* __ASM_UNICORE64_IRQFLAGS_H__ */
