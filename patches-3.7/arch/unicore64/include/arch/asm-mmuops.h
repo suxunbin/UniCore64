4d45dbc616949811c111efbd7637f556fb7902aa
LiangRongrong <Liangrongrong@mprc.pku.edu.cn>
2011-12-05 11:01:07 +0800

    UniCore64: Modify cp0 to p0 in arch asm-mmuops.h
    Signed-off-by: Liang Rongrong <liangrongrong@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
+++ patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
@@ -8,7 +8,7 @@
  */
 __ASMMACRO_WRAP(.macro uc64_invalidate_dcache, rt;
 			dmovl	&rt, #0;
-			movc	cp0.c6, &rt, #8;
+			movc	p0.c6, &rt, #8;
 		.endm)
 
 /**
@@ -16,7 +16,7 @@ __ASMMACRO_WRAP(.macro uc64_invalidate_dcache, rt;
  */
 __ASMMACRO_WRAP(.macro uc64_flush_dcache, rt;
 			dmovl	&rt, #0;
-			movc	cp0.c6, &rt, #12;
+			movc	p0.c6, &rt, #12;
 		.endm)
 
 /**
@@ -24,7 +24,7 @@ __ASMMACRO_WRAP(.macro uc64_flush_dcache, rt;
  */
 __ASMMACRO_WRAP(.macro uc64_invalidate_icache, rt;
 			dmovl	&rt, #0;
-			movc	cp0.c7, &rt, #0;
+			movc	p0.c7, &rt, #0;
 		.endm)
 
 /**
@@ -32,7 +32,7 @@ __ASMMACRO_WRAP(.macro uc64_invalidate_icache, rt;
  */
 __ASMMACRO_WRAP(.macro uc64_invalidate_tlb, rt;
 			dmovl	&rt, #0;
-			movc	cp0.c5, &rt, #24;
+			movc	p0.c5, &rt, #24;
 		.endm)
 
 #endif /* __UNICORE64_ARCH_ASM_MMUOPS_H__ */
