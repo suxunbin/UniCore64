6347abdf27fcd503f80d9607675dbc4a099a97bc
Liu Guoli <liuguoli@mprc.pku.edu.cn>
2011-12-09 17:47:56 +0800

    UniCore64: Add block doc for irqflags.h
    Signed-off-by: Liu Guoli <liuguoli@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/irqflags.h
+++ patches-3.7/arch/unicore64/include/asm/irqflags.h
@@ -1,6 +1,24 @@
 #ifndef __UNICORE64_ASM_IRQFLAGS_H__
 #define __UNICORE64_ASM_IRQFLAGS_H__
 
+/**
+ * DOC: IRQFLAGS_H
+ * Code for interrupt control specific to PKUnity SoC and UniCore ISA.
+ * Use the generic function for simple life.
+ *
+ * \\\\lt:programlisting\\\\gt:
+ * -
+ * arch_local_save_flags():		read interrupt enabled status
+ * arch_local_irq_restore():	set interrupt enabled status
+ * arch_local_irq_save():		get status and disable interrupts
+ * arch_irqs_disabled_flags():	test flags
+ * arch_local_irq_enable():		unconditionally enable interrupts
+ * arch_local_irq_disable():	unconditionally disable interrupts
+ * arch_irqs_disabled():		test hardware interrupt enable bit
+ * -
+ * \\\\lt:/programlisting\\\\gt:
+ */
+
 #include <arch/hwdef-cpu.h>
 
 #define ARCH_IRQ_DISABLED	ASR_INTR_MASK
