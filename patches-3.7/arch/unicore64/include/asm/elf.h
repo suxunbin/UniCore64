d4e1d9121e6f0fd2f7a2fdcee7602ea631b70edf
GuanXuetao <gxt@mprc.pku.edu.cn>
2011-12-10 15:40:36 +0800

    UniCore64: remove unnecessary progmas and adjust comments format in asm/elf.h
    Signed-off-by: GuanXuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/elf.h
+++ patches-3.7/arch/unicore64/include/asm/elf.h
@@ -15,39 +15,35 @@ typedef elf_greg_t elf_gregset_t[ELF_NGREG];
 #define ELF_PLATFORM		(NULL)
 #define ELF_EXEC_PAGESIZE	PAGE_SIZE
 
-#ifndef __ASSEMBLY__
-
 #define FP_REGS_NUMBER		33
 
 struct fpu_regs {
 	unsigned int regs[FP_REGS_NUMBER];
 };
 
-#endif
-
 typedef struct fpu_regs elf_fpregset_t;
 
-#if defined(__KERNEL__) && !defined(__ASSEMBLY__)
 /*
-* This yields a mask that user programs can use to figure out what
-* instruction set this cpu supports.
-*/
+ * This yields a mask that user programs can use to figure out what
+ * instruction set this cpu supports.
+ */
 #define ELF_HWCAP              0
-#endif
 
 /*
-* This is used to ensure we don't load something for the wrong architecture.
-*/
+ * This is used to ensure we don't load something for the wrong architecture.
+ */
 #define elf_check_arch(x)  \
 	((x)->e_ident[EI_CLASS] == ELF_CLASS && \
 	(x)->e_machine == EM_UNICORE64)
 
 #define SET_PERSONALITY(ex) set_personality(PER_LINUX)
 
-/* This is the location that an ET_DYN program is loaded if exec'ed.  Typical
-   use of this is to invoke "./ld.so someprog" to test out a new version of
-   the loader.  We need to make sure that it is out of the way of the program
-   that it will "exec", and that there is sufficient room for the brk.  */
+/*
+ * This is the location that an ET_DYN program is loaded if exec'ed.  Typical
+ * use of this is to invoke "./ld.so someprog" to test out a new version of
+ * the loader.  We need to make sure that it is out of the way of the program
+ * that it will "exec", and that there is sufficient room for the brk.
+ */
 
 #define ELF_ET_DYN_BASE	(2 * TASK_SIZE / 3)
 
