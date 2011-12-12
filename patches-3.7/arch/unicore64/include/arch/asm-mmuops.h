f1a18f3fdfab0153cf58df4080a4c2d58c2c3659
yuyue <yuyue@mprc.pku.edu.cn>
2011-12-12 13:56:17 +0800

    UniCore64: delete enable cache macros in asm-mmuops.h
    Signed-off-by: yuyue <yuyue@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
+++ patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
@@ -36,28 +36,6 @@ __ASMMACRO_WRAP(.macro	uc64_invalidate_tlb, rt;
 		.endm)
 
 /**
- * uc64_enable_dcache -  enable dcache
- */
-__ASMMACRO_WRAP(.macro	uc64_enable_dcache, rt;
-#ifndef CONFIG_CPU_DCACHE_DISABLE
-			movc	&rt, p0.c1, #0;
-			dor	&rt, &rt, #4;
-			movc	p0.c1, &rt, #0;
-#endif
-		.endm)
-
-/**
- * uc64_enable_icache -  enable icache
- */
-__ASMMACRO_WRAP(.macro	uc64_enable_icache, rt;
-#ifndef CONFIG_CPU_ICACHE_DISABLE
-			movc	&rt, p0.c1, #0;
-			dor	&rt, &rt, #8;
-			movc	p0.c1, &rt, #0;
-#endif
-		.endm)
-
-/**
  * uc64_set_pgd -  set pgd address
  */
 __ASMMACRO_WRAP(.macro	uc64_set_pgd, rpgd;
