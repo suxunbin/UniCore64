90d43d14f1555b03e1d0f6dfbdf270453d3406d7
Guan Xuetao <gxt@mprc.pku.edu.cn>
2011-12-13 11:58:38 +0800

    UniCore64: make arch-specific memchr, memcpy, memmove and memset available
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

---
+++ patches-3.7/arch/unicore64/include/asm/string.h
@@ -0,0 +1,16 @@
+#ifndef __UNICORE64_ASM_STRING_H__
+#define __UNICORE64_ASM_STRING_H__
+
+#define __HAVE_ARCH_MEMCPY
+extern void *memcpy(void *, const void *, __kernel_size_t);
+
+#define __HAVE_ARCH_MEMMOVE
+extern void *memmove(void *, const void *, __kernel_size_t);
+
+#define __HAVE_ARCH_MEMCHR
+extern void *memchr(const void *, int, __kernel_size_t);
+
+#define __HAVE_ARCH_MEMSET
+extern void *memset(void *, int, __kernel_size_t);
+
+#endif /* __UNICORE64_ASM_STRING_H__ */
