776a2e8ec1ab48332322ad3210aa7cd332c99e51
LiangRongrong <Liangrongrong@mprc.pku.edu.cn>
2011-12-15 15:48:21 +0800

    UniCore64: Add kernel-doc for arch asm-mmuops.h
    Signed-off-by: Liang Rongrong <liangrongrong@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
+++ patches-3.7/arch/unicore64/include/arch/asm-mmuops.h
@@ -1,9 +1,15 @@
 #ifndef __UNICORE64_ARCH_ASM_MMUOPS_H__
 #define __UNICORE64_ARCH_ASM_MMUOPS_H__
 
+/**
+ * DOC: ASM_MMUOPS_H
+ * This file defines MMU wraps such as invalidate icache,
+ * dcache, or tlb.
+ */
 #include <arch/asm-common.h>
 
 /**
+ * DOC: ASM_MMUOPS_H_INVALIDATE_DCACHE
  * uc64_invalidate_dcache - invalidate entire L1 dcache
  */
 __ASMMACRO_WRAP(.macro	uc64_invalidate_dcache, rt;
@@ -12,6 +18,7 @@ __ASMMACRO_WRAP(.macro	uc64_invalidate_dcache, rt;
 		.endm)
 
 /**
+ * DOC: ASM_MMUOPS_H_FLUSH_DCACHE
  * uc64_flush_dcache - flush entire L1 dcache
  */
 __ASMMACRO_WRAP(.macro	uc64_flush_dcache, rt;
@@ -20,6 +27,7 @@ __ASMMACRO_WRAP(.macro	uc64_flush_dcache, rt;
 		.endm)
 
 /**
+ * DOC: ASM_MMUOPS_H_INVALIDATE_ICACHE
  * uc64_invalidate_icache - invalidate entire icache
  */
 __ASMMACRO_WRAP(.macro	uc64_invalidate_icache, rt;
@@ -28,6 +36,7 @@ __ASMMACRO_WRAP(.macro	uc64_invalidate_icache, rt;
 		.endm)
 
 /**
+ * DOC: ASM_MMUOPS_H_INVALIDATE_TLB
  * uc64_invalidate_tlb - invalidate I&D tlb
  */
 __ASMMACRO_WRAP(.macro	uc64_invalidate_tlb, rt;
@@ -36,6 +45,7 @@ __ASMMACRO_WRAP(.macro	uc64_invalidate_tlb, rt;
 		.endm)
 
 /**
+ * DOC: ASM_MMUOPS_H_SET_PGD
  * uc64_set_pgd -  set pgd address
  */
 __ASMMACRO_WRAP(.macro	uc64_set_pgd, rpgd;
