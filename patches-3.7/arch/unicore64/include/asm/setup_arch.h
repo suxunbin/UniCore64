c7011c919b884a1d134229cb78153b6d4aa302b4
Guan Xuetao <gxt@mprc.pku.edu.cn>
2012-01-14 14:07:01 +0800

    UniCore64: add trap_init handler
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/setup_arch.h
+++ patches-3.7/arch/unicore64/include/asm/setup_arch.h
@@ -1,9 +1,6 @@
 #ifndef __UNICORE64_ASM_SETUP_ARCH_H__
 #define __UNICORE64_ASM_SETUP_ARCH_H__
 
-#include <linux/stringify.h>
-#include <arch/hwdef-copro.h>
-
 extern void setup_arch_cpuinfo(void);
 extern void setup_arch_memory(void);
 
@@ -14,5 +11,7 @@ extern void setup_arch_devtree(char *cmdline);
 #else
 #define setup_arch_devtree(s)		do { } while (0)
 #endif /* CONFIG_OF_EARLY_FLATTREE */
+
+extern char __vectors_table[];
 
 #endif /* __UNICORE64_ASM_SETUP_ARCH_H__ */
--- patches-3.7/arch/unicore64/kernel/traps.c
+++ patches-3.7/arch/unicore64/kernel/traps.c
@@ -1,13 +1,16 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <asm/setup_arch.h>
+
+#include <arch/hwdef-cp0-sysctrl.h>
+
 /**
  * trap_init() -
  */
 void __init trap_init(void)
 {
-	/* FIXME */
-	BUG();
+	write_cp(__vectors_table, CP0_INTR_VECBASE);
 }
 
 /**
