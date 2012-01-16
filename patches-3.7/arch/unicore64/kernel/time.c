41525d07b2864d98de6ed1d8081db73a9811b300
Guan Xuetao <gxt@mprc.pku.edu.cn>
2012-01-16 13:20:30 +0800

    UniCore64: hook the uc64_inner_timer_irqhandler
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/kernel/time.c
+++ patches-3.7/arch/unicore64/kernel/time.c
@@ -6,7 +6,6 @@
 #include <linux/clockchips.h>
 
 #include <arch/hwdef-copro.h>
-#include <arch/hwdef-irq.h>
 
 #define INNER_TIMER_MATCHREG		p1.c0
 #define INNER_TIMER_COUNTER		p1.c1
@@ -63,30 +62,11 @@ static void uc64_inner_timer_set_mode(enum clock_event_mode mode,
 	}
 }
 
-static irqreturn_t uc64_inner_timer_irqhandler(int irq, void *dev_id)
-{
-	struct clock_event_device *c = dev_id;
-
-	/* Disarm the compare/match, signal the event. */
-	uc64_inner_timer_irq_disable();
-	uc64_inner_timer_status_clear();
-	c->event_handler(c);
-
-	return IRQ_HANDLED;
-}
-
 static struct clock_event_device uc64_inner_timer_ce = {
 	.name		= "uc64-inner-timer-clock-event-device",
 	.features	= CLOCK_EVT_FEAT_ONESHOT,
 	.set_next_event	= uc64_inner_timer_set_next_event,
 	.set_mode	= uc64_inner_timer_set_mode,
-};
-
-static struct irqaction uc64_inner_timer_irq = {
-	.name		= "uc64-inner-timer-irq",
-	.flags		= IRQF_TIMER | IRQF_IRQPOLL,
-	.handler	= uc64_inner_timer_irqhandler,
-	.dev_id		= &uc64_inner_timer_ce,
 };
 
 static struct clocksource uc64_inner_timer_cs = {
@@ -95,6 +75,15 @@ static struct clocksource uc64_inner_timer_cs = {
 	.mask		= CLOCKSOURCE_MASK(64),
 	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
 };
+
+void uc64_inner_timer_irqhandler(void)
+{
+	/* Disarm the compare/match, signal the event. */
+	uc64_inner_timer_irq_disable();
+	uc64_inner_timer_status_clear();
+
+	uc64_inner_timer_ce.event_handler(&uc64_inner_timer_ce);
+}
 
 /**
  * time_init() -
@@ -106,8 +95,5 @@ void __init time_init(void)
 	uc64_inner_timer_status_clear();
 
 	clockevents_calc_mult_shift(&uc64_inner_timer_ce, CLOCK_TICK_RATE, 5);
-
-	setup_irq(IRQ_TIMER0, &uc64_inner_timer_irq);
-
 	clocksource_register_hz(&uc64_inner_timer_cs, CLOCK_TICK_RATE);
 }
