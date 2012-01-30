61c30131d6f57f7cad6d50cd6b6f2eecb8d0b20a
Guan Xuetao <gxt@mprc.pku.edu.cn>
2012-01-30 15:11:03 +0800

    UniCore64: disable and clear all uc64 interrupts in init_IRQ
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/kernel/irq.c
+++ patches-3.7/arch/unicore64/kernel/irq.c
@@ -1,11 +1,15 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <arch/hwdef-cpu.h>
+#include <arch/hwdef-cp0-sysctrl.h>
+
 /**
  * init_IRQ() -
  */
 void __init init_IRQ(void)
 {
-	/* FIXME */
-	BUG();
+	/* Disable and clear all uc64 interrupts */
+	__write_uc64(__read_uc64(asr) | ASR_INTR_SELECT, asr);
+	__write_cp(0, CP0_INTR);
 }
