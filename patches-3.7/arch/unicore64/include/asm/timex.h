4e9c0898c3d70b34ad54a30c830a5496693ed89e
Guan Xuetao <gxt@mprc.pku.edu.cn>
2012-05-16 16:00:04 +0800

    UniCore64: set timer frequency to 10 times per second for fpga
    Signed-off-by: Chang Huaixin <changhuaixin@mprc.pku.edu.cn>
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/timex.h
+++ patches-3.7/arch/unicore64/include/asm/timex.h
@@ -1,9 +1,15 @@
 #ifndef __UNICORE64_ASM_TIMEX_H__
 #define __UNICORE64_ASM_TIMEX_H__
 
+#include <asm/sizes.h>
+
 #ifdef CONFIG_ARCH_FPGA
-/* in FPGA, OST clock is 32K */
-#   define CLOCK_TICK_RATE		(32*1024)
+/*
+ * In FPGA, itimer clock is 5M. It takes too much time to handle regular timer
+ * interrupts in a second. Set CLOCK_TICK_RATE at X*5M to have 10 interrupts
+ * a second instead.
+ */
+#	define CLOCK_TICK_RATE		((CONFIG_HZ / 10) * (5 * SZ_1M))
 #endif /* CONFIG_ARCH_FPGA */
 
 #include <asm-generic/timex.h>
