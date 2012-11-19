aa932befd43a4d61526e3f9558de55d4504dcbb8
Liu Guoli <liuguoli@mprc.pku.edu.cn>
2012-11-19 15:53:07 +0800

    UniCore64: Add do_notify_resume
    Signed-off-by: Liu Guoli <liuguoli@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/kernel/signal.c
+++ patches-3.7/arch/unicore64/kernel/signal.c
@@ -1,4 +1,5 @@
 #include <linux/syscalls.h>
+#include <linux/tracehook.h>
 
 SYSCALL_DEFINE3(sigaltstack, const stack_t __user *, uss,
 		stack_t __user *, uoss,	struct pt_regs *, regs)
@@ -11,3 +12,14 @@ SYSCALL_DEFINE1(rt_sigreturn, struct pt_regs *, regs)
 	BUG();
 }
 
+asmlinkage void do_notify_resume(struct pt_regs *regs,
+		unsigned int thread_flags, int syscall)
+{
+	if (thread_flags & (1 << TIF_SIGPENDING))
+		BUG();
+
+	if (thread_flags & (1 << TIF_NOTIFY_RESUME)) {
+		clear_thread_flag(TIF_NOTIFY_RESUME);
+		tracehook_notify_resume(regs);
+	}
+}
