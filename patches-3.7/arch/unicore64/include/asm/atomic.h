3b47c092c28775da7d7a9d49c056db9b89fd5c3e
Chang Huaixin <changhuaixin@mprc.pku.edu.cn>
2012-12-15 15:31:49 +0800

    unicore64: Add atomic_add_return and atomic_sub_return
    Signed-off-by: Chang Huaixin <changhuaixin@mprc.pku.edu.cn>

--- patches-3.7/arch/unicore64/include/asm/atomic.h
+++ patches-3.7/arch/unicore64/include/asm/atomic.h
@@ -1,24 +1,50 @@
 #ifndef __UNICORE64_ASM_ATOMIC_H__
 #define __UNICORE64_ASM_ATOMIC_H__
 
+#include <asm/barrier.h>
+
 #include <arch/asm-debug.h>
 
 #ifdef CONFIG_SMP
 #ifndef atomic_add_return
-#define atomic_add_return(i, v)		\
-	({					\
-		(v)->counter += i;		\
-		uc64_debug_putx(0xdead0011);	\
-		0;				\
+#define atomic_add_return(i, v)					\
+	({								\
+		unsigned long tmp;					\
+		int result;						\
+		smp_mb();						\
+		__asm__ __volatile__(					\
+			"1:	llw		%0, [%2+], #0\n"	\
+			"	add		%0, %0, %3\n"		\
+			"	mov		%1, %0\n"		\
+			"	scw		%1, [%2+], #0\n"	\
+			"	cmpsub.a	%1, #0\n"		\
+			"	beq		1b"			\
+			: "=&r" (result), "=&r" (tmp)			\
+			: "r" (&(v)->counter), "r" (i)			\
+			: "cc", "memory");				\
+		smp_mb();						\
+		result;							\
 	})
 #endif
 
 #ifndef atomic_sub_return
-#define atomic_sub_return(i, v)		\
-	({					\
-		(v)->counter -= i;		\
-		uc64_debug_putx(0xdead0012);	\
-		0;				\
+#define atomic_sub_return(i, v)					\
+	({								\
+		unsigned long tmp;					\
+		int result;						\
+		smp_mb();						\
+		__asm__ __volatile__(					\
+			"1:	llw		%0, [%2+], #0\n"	\
+			"	sub		%0, %0, %3\n"		\
+			"	mov		%1, %0\n"		\
+			"	scw		%1, [%2+], #0\n"	\
+			"	cmpsub.a	%1, #0\n"		\
+			"	beq		1b"			\
+			: "=&r" (result), "=&r" (tmp)			\
+			: "r" (&(v)->counter), "r" (i)			\
+			: "cc", "memory");				\
+		smp_mb();						\
+		result;							\
 	})
 #endif
 
