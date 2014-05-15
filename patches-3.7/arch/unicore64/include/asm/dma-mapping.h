8eceab6d368046bc0fd8bc2f0edbe1c297db566f
Zhan Yuefeng <zhanyuefeng@mprc.pku.edu.cn>
2014-05-15 08:57:28 +0800

    UniCore64: Force to use swiotlb

--- patches-3.7/arch/unicore64/include/asm/dma-mapping.h
+++ patches-3.7/arch/unicore64/include/asm/dma-mapping.h
@@ -31,10 +31,9 @@ extern struct dma_map_ops swiotlb_dma_map_ops;
 
 static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
 {
-	if (dev && dev->dma_mask)
-		return addr + size - 1 <= *dev->dma_mask;
+	/* Force to use swiotlb */
 
-	return 1;
+	return 0;
 }
 
 static inline dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
